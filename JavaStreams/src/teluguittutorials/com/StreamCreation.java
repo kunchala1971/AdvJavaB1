a1c1cc98de2def977d6e3924e528d19b4709cbd4
kunchala1971 <kunchala1971@gmail.com>
2026-09-10 10:44:43 +0530

    Streams start

--- JavaStreams/src/teluguittutorials/com/StreamCreation.java
+++ JavaStreams/src/teluguittutorials/com/StreamCreation.java
@@ -1,10 +1,32 @@
 package teluguittutorials.com;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamCreation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Collection(List,Set)
+		List<String> list=Arrays.asList("Apple","Bannan","Cherry");
+		Stream<String> streamFromList=list.stream();
+		System.out.println("Stream From List Count "+ streamFromList.count());
+		System.out.println("Stream From List Hascode "+ streamFromList.hashCode());
+		//From an Array
+		String[] array= {"onge","two","three"};
+		Stream<String> streamFromArray=Arrays.stream(array);
+		System.out.println("Stream From Array Count "+ streamFromArray.count());
+		
+		//From Individual Values using stream.of()
+		Stream<Integer> streamOfValues=Stream.of(1,2,3,4,5,6);
+		System.out.println("Stream From Individual elements Count "+ streamOfValues.count());
+		
+		//Stream.generate() for infinite streams (need limit)
+		Stream<Double> randomStream=Stream.generate(Math::random).limit(5);
+		randomStream.forEach(value->System.out.print(value + ", "));
+		System.out.println(" ");
+	
 	}
 
 }
